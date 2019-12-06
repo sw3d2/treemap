fc9512208682483eb4651cd4e1cbda494fc2f769
sw3d <>
2019-12-06 00:35:53 -0800

    node.js

--- index.js
+++ index.js
@@ -1,52 +1,72 @@
 'use strict';
 
-const margin = {top: 40, right: 10, bottom: 10, left: 10},
-      width = 960 - margin.left - margin.right,
-      height = 500 - margin.top - margin.bottom,
-      color = d3.scaleOrdinal().range(d3.schemeCategory20c);
+const WEB = typeof XMLHttpRequest !== 'undefined';
+const d3 = WEB ? window.d3 : require('d3');
 
-const treemap = d3.treemap().size([width, height]);
+async function makeTreeMap(jsonFilePath = 'flare.json') {
+  const margin = { top: 40, right: 10, bottom: 10, left: 10 };
+  const width = 960 - margin.left - margin.right;
+  const height = 500 - margin.top - margin.bottom;
+  const color = d3.scaleOrdinal().range(d3.schemeCategory20c);
+  const treemap = d3.treemap().size([width, height]);
 
-const div = d3.select("body").append("div")
+  const data = await readJson(jsonFilePath);
+  console.log('json:', data);
+
+  const root = d3.hierarchy(data, (d) => d.children)
+    .sum((d) => d.size);
+
+  const tree = treemap(root);
+  console.log('tree:', tree);
+
+  const div = d3.select("body").append("div")
     .style("position", "relative")
     .style("width", (width + margin.left + margin.right) + "px")
     .style("height", (height + margin.top + margin.bottom) + "px")
     .style("left", margin.left + "px")
     .style("top", margin.top + "px");
 
-d3.json("flare.json", function(error, data) {
-  if (error) throw error;
-
-  const root = d3.hierarchy(data, (d) => d.children)
-    .sum((d) => d.size);
-
-  const tree = treemap(root);
-
   const node = div.datum(root).selectAll(".node")
-      .data(tree.leaves())
+    .data(tree.leaves())
     .enter().append("div")
-      .attr("class", "node")
-      .style("left", (d) => d.x0 + "px")
-      .style("top", (d) => d.y0 + "px")
-      .style("width", (d) => Math.max(0, d.x1 - d.x0 - 1) + "px")
-      .style("height", (d) => Math.max(0, d.y1 - d.y0  - 1) + "px")
-      .style("background", (d) => color(d.parent.data.name))
-      .text((d) => d.data.name);
+    .attr("class", "node")
+    .style("left", (d) => d.x0 + "px")
+    .style("top", (d) => d.y0 + "px")
+    .style("width", (d) => Math.max(0, d.x1 - d.x0 - 1) + "px")
+    .style("height", (d) => Math.max(0, d.y1 - d.y0 - 1) + "px")
+    .style("background", (d) => color(d.parent.data.name))
+    .text((d) => d.data.name);
 
   d3.selectAll("input").on("change", function change() {
     const value = this.value === "count"
-        ? (d) => { return d.size ? 1 : 0;}
-        : (d) => { return d.size; };
+      ? (d) => { return d.size ? 1 : 0; }
+      : (d) => { return d.size; };
 
     const newRoot = d3.hierarchy(data, (d) => d.children)
       .sum(value);
 
     node.data(treemap(newRoot).leaves())
       .transition()
-        .duration(1500)
-        .style("left", (d) => d.x0 + "px")
-        .style("top", (d) => d.y0 + "px")
-        .style("width", (d) => Math.max(0, d.x1 - d.x0 - 1) + "px")
-        .style("height", (d) => Math.max(0, d.y1 - d.y0  - 1) + "px")
+      .duration(1500)
+      .style("left", (d) => d.x0 + "px")
+      .style("top", (d) => d.y0 + "px")
+      .style("width", (d) => Math.max(0, d.x1 - d.x0 - 1) + "px")
+      .style("height", (d) => Math.max(0, d.y1 - d.y0 - 1) + "px")
   });
-});
+}
+
+async function readJson(filepath) {
+  if (WEB) {
+    return await new Promise((resolve, reject) =>
+      d3.json(filepath, (err, res) =>
+        err ? reject(err) : resolve(res)))
+  } else {
+    const fs = require('fs');
+    const json = await new Promise((resolve, reject) =>
+      fs.readFile(filepath, 'utf8',
+        (err, res) => err ? reject(err) : resolve(res)));
+    return JSON.parse(json);
+  }
+}
+
+makeTreeMap(WEB ? undefined : process.argv[2]);
